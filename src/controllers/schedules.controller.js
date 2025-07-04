b02b53114220928dc5365ed694cf16c5ec2efe5e
IgnaRamos <ignacio1773@gmail.com>
2025-07-03 20:38:16 -0400

    Update schedules.controller.js

--- src/controllers/schedules.controller.js
+++ src/controllers/schedules.controller.js
@@ -18,8 +18,7 @@ const getHistoryByRut = async (req, res) => {
         JOIN exams     e ON s.exam_id      = e.id
         JOIN patients  p ON s.patient_id   = p.id
       WHERE p.rut = ?
-      AND sh.new_status = 'cancelled'
-      AND sh.new_status = 'confirmed'
+      AND (sh.new_status = 'cancelled' OR sh.new_status = 'pending') 
       ORDER BY sh.changed DESC
     `, [rut]);
     res.json(rows);
