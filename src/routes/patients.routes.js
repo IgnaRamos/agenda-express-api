d10784f8f9b70a89b76a61d549da7494da42c43a
IgnaRamos <ignacio1773@gmail.com>
2025-07-08 00:15:53 -0400

    Update patients.routes.js

--- src/routes/patients.routes.js
+++ src/routes/patients.routes.js
@@ -5,6 +5,7 @@ const { verifyToken, isAdmin, isScheduler } = require('../middleware/auth');
 
 router.use(verifyToken, isAdmin, isScheduler);
 
+router.get('/', patientsCtrl.getAll)
 router.get('/:rut', patientsCtrl.getByRut);
 router.post('/', patientsCtrl.createPatient);
 router.put('/:rut', patientsCtrl.updatePatient);
