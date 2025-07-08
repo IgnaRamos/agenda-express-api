3ede1850ed285ddaf3541aa66d89cc61932732f4
IgnaRamos <ignacio1773@gmail.com>
2025-07-08 00:20:25 -0400

    Update patients.routes.js

--- src/routes/patients.routes.js
+++ src/routes/patients.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const patientsCtrl = require('../controllers/patients.controller');
-const { verifyToken, isAdmin, isScheduler } = require('../middleware/auth');
+const { verifyToken, isAdmin } = require('../middleware/auth');
 
 router.use(verifyToken, isAdmin);
-router.use(verifyToken, isScheduler);
+
 
 router.get('/', patientsCtrl.getAll)
 router.get('/:rut', patientsCtrl.getByRut);
